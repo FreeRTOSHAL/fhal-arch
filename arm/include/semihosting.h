2368c09ee43231d6302c1a5f75e10b2be2014646
Andreas Werner <kernel@andy89.org>
2018-03-19 00:36:50 +0100

    add uart global array dev and add UART dependency for ARM Semihosting

--- arm/include/semihosting.h
+++ arm/include/semihosting.h
@@ -2,7 +2,10 @@
 #define SEMIHOSTING_H_
 #include <stdint.h>
 #include <stdbool.h>
+#include <hal.h>
+#include <uart.h>
 bool semihosting_debugIsEnabled();
 uint32_t semihosting_systemCall(uint32_t r1, uint32_t r2);
+HAL_DEFINE_GLOBAL_ARRAY(uart);
 #define SEMIHOSTING_UART_ID HAL_GET_ID(uart, semihosting, semihosting_uart)
 #endif
